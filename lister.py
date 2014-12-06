3da8ff0923f958d0ca2ccfa678960d05574217ea
Uri Bushey <ubushey@appnexus.com>
2014-12-06 23:09:08 +0000

    Removed a debug print statement.

--- lister.py
+++ lister.py
@@ -13,7 +13,6 @@ TEAMS_API_URL = 'https://www.eventbriteapi.com/v3/events/{}/teams?token={}'
 def list_teams():
     r = requests.get(TEAMS_API_URL.format(EVENT_ID,TOKEN))
     teams_data = loads(r.content)
-    print teams_data
     return render_template('teams.html', teams = teams_data)
 
 if __name__ == '__main__':
